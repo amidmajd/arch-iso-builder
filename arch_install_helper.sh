332d9665ebdf2d7a52592fab6a0d674d9433d9f3
Amid Majd <amidmajd@gmail.com>
2021-08-06 13:24:25 +0430

    KDE packages fixed + bug fixes in arch_install_helper.sh

--- arch_install_helper.sh
+++ arch_install_helper.sh
@@ -15,7 +15,7 @@ mkdir /mnt/home
 mount /dev/vda1 /mnt/boot/efi
 mount -o noatime /dev/vda4 /mnt/home
 
-reflector --country United_Kingdom,Germany  --verbose --latest 10 -a 12 --protocol http,https --sort rate --save /etc/pacman.d/mirrorlist
+reflector --country United_Kingdom,Germany  --verbose --latest 30 -a 12 --protocol http,https --sort rate --save /etc/pacman.d/mirrorlist
 pacman -Sy
 pacstrap /mnt base linux-zen linux-zen-headers linux-firmware reflector vim htop
 cp -f /etc/pacman.conf /mnt/etc/pacman.conf
@@ -27,7 +27,7 @@ arch-chroot /mnt
 ### after arch-chroot ###
 ln -sf /usr/share/zoneinfo/Asia/Tehran /etc/localtime
 hwclock --systohc
-sed -i 's/#en_US.UTF-8/en_US.UTF-8/' locale.gen     # setting locale
+sed -i 's/#en_US.UTF-8/en_US.UTF-8/' /etc/locale.gen     # setting locale
 sed -i 's/#fa_IR/fa_IR/' /etc/locale.gen
 locale-gen
 echo "LANG=en_US.UTF-8" >> /etc/locale.conf
@@ -59,17 +59,17 @@ grub-install --recheck --target=x86_64-efi --bootloader-id=Arch
 grub-mkconfig -o /boot/grub/grub.cfg
 
 
-pacman -S networkmanager network-manager-applet networkmanager-openvpn wireless_tools wpa_supplicant dialog blueberry cups
+pacman -S networkmanager networkmanager-openvpn wireless_tools wpa_supplicant dialog blueberry cups
 pacman -S xorg
-pacman -S alacritty firefox net-tools neofetch
-systemctl enable NetworkManager bluetooth cups
+pacman -S alacritty firefox net-tools neofetch openssh
+systemctl enable NetworkManager bluetooth cups fstrim.timer
 
 # Desktops
 pacman -S cinnamon gnome-keyring xdg-user-dirs
 pacman -S gnome gdm gnome-extra
-pacman -S plasma-meta sddm plasma-wayland-session kde-system-meta konsole kdeconnect dolphin-plugins kwalletmanager ark lrzip lzop p7zip unrar unarchiver filelight kcalc gwenview kcolorchooser okular kdenlive elisa kdenetwork-filesharing print-manager
+pacman -S plasma-desktop sddm sddm-kcm kwayland-integration plasma-wayland-session konsole dolphin dolphin-plugins kdeconnect kcron ksystemlog plasma-disks plasma-systemmonitor kwalletmanager kwallet-pam gparted gnome-disk-utility ark lrzip lzop p7zip unrar unarchiver filelight kcalc gwenview okular kdenlive elisa kdenetwork-filesharing print-manager bluedevil breeze-gtk drkonqi kde-gtk-config kdeplasma-addons kgamma5 khotkeys kinfocenter kscreen ksshaskpass kwrited plasma-browser-integration plasma-firewall plasma-nm plasma-pa plasma-thunderbolt plasma-workspace-wallpapers powerdevil xdg-desktop-portal-kde
 
-
+# Login Managers
 pacman -S lightdm lightdm-gtk-greeter
 vim /etc/lightdm/lightdm.conf
 # greeter-session=lightdm-gtk-greeter
@@ -79,7 +79,6 @@ systemctl enable lightdm | gdm | sddm
 
 # VMs
 # fix picom for VMs: vim /etc/xdg/picom.conf  ==> comment "vsync" option
-pacman -S openssh
 vim /etc/ssh/sshd_config       # PermitRootLogin yes
 systemctl enable sshd
 
